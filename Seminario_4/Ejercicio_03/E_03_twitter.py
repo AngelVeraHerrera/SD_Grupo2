01066df1a0cf8469fc3333b9ed5c11d4bba90c78
AngelVeraHerrera <angeldelaveracruz@gmail.com>
2015-04-14 16:06:59 +0200

    Actualizando

--- Seminario_4/Ejercicio_03/E_03_twitter.py
+++ Seminario_4/Ejercicio_03/E_03_twitter.py
@@ -10,22 +10,17 @@ from flask import Flask, render_template
 
 from flask_googlemaps import GoogleMaps
 
+from flask import request
+
 from flask_googlemaps import Map
 
-import utilidades_varias
+import json
 
-def menu():
-    option = 0
-    utilidades_varias.clear() # Clear the terminal
-    print '***************************************'
-    print '**     "BÚSQUEDA DE TWITS" v2        **'
-    print '**       by A. Vera & A.Pastor       **'
-    print '***************************************'
-    print '* Introduzca un tema para buscar:     *'
-    print '***************************************'
-    tema = raw_input('Tema -> ')
-    utilidades_varias.clear()
-    return tema
+import twitter
+
+import io
+
+import utilidades_varias
 
 ''' 
 -------------------------------------------
@@ -33,36 +28,45 @@ def menu():
 -------------------------------------------
 '''
 N_TWITS = 1000 #Contador de twits. Se almacenan hasta 1000 twits.
-
-tema = menu()
-l_coord = []
 app = Flask(__name__)
 GoogleMaps(app)
-    
-twitter_api = utilidades_varias.login_twitter() 
-results = twitter_api.search.tweets(q = tema, count = N_TWITS, geocode='36.516667,-6.283333,1000km')
-utilidades_varias.save_json("twits",results) 
 
-with open('twits.json') as data_file: 
-	data = json.load(data_file)
+def search(tema):
 
+    l_coord = []
 
-for estado in data["statuses"]: 
-	if estado["geo"]:
-		coord = estado["coordinates"]
-		xy=[coord.values()[1][1] , coord.values()[1][0]]
-		l_coord.append(xy) 
+    twitter_api = utilidades_varias.login_twitter() 
+    results = twitter_api.search.tweets(q = tema, count = N_TWITS, geocode='40.25,-3.45,10000km')
+    utilidades_varias.save_json("twits",results) 
 
-@app.route("/map")
+    with open('twits.json') as data_file: 
+	    data = json.load(data_file)
+
+    for estado in data["statuses"]: 
+    	if estado["geo"]:
+    		coord = estado["coordinates"]
+    		xy=[coord.values()[1][1] , coord.values()[1][0]]
+    		l_coord.append(xy) 
+
+    return l_coord
+
+@app.route("/map", methods=['POST'])
 def mapview():
-	mymap = Map( 
-		identifier="view-side",
-		lat = 36.516667,
-		lng = -6.283333,
-		markers = lista,
+    tema = request.form['text']
+    coord = search(tema)
+    mymap = Map(
+        identifier="view-side",
+		lat = 40.3450396,
+		lng = -3.6517684,
+        zoom=6,
+		markers = coord,
 		style="height:800px;width:800px;margin:0;"
 	)
-	return render_template('map.html', mymap=mymap) 
+    return render_template('map.html', mymap=mymap) 
+
+@app.route("/")
+def index():
+    return render_template('index.html') 
 
 if __name__ == "__main__":
     app.run(debug=True)
